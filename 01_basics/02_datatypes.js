3d9451d089646a969a6f3eff29a974c3019c104f
jatin4114 <jatinpanghal007@gmail.com>
2023-09-27 11:46:51 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat JS as new version of JS
+
+// alert(3+3) // we are using nodejs, not browser
+
+// high code readability
+
+let name = "HITESH"
+let age =18
+let isLoggedIn = false
+
+//datatypes
+
+// number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null=> standalone value
+//undefined =>
+//symbol=> unique
+//object
+
+console.log(typeof null);
---
+++ 01_basics/variable.js
@@ -0,0 +1,21 @@
+console.log("jatin")
+
+const accountId = 1234
+let accountEmail ="hitesh@gmail.com"
+var accountPsssword = "1234"
+accountCity = "jaipur"
+let accountState;
+// accountId=2 not allowed
+accountEmail ="hc.com"
+accountPsssword='234543'
+accountCity ="manipal"
+console.log(accountId);
+console.table([accountId, accountEmail,accountPsssword ,accountCity])
+
+/*
+never use var because of scope problem
+*/
+
+console.log(accountState)
+
+
